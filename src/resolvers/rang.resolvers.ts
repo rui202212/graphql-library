aa7171e8fd655b84a19a8c0f3d58311de5e41e38
rui202212 <lvrui.lurui@gmail.com>
2026-03-05 22:41:10 +0000

    feature/resolvers: add rang resolvers

---
+++ src/resolvers/rang.resolvers.ts
@@ -0,0 +1,24 @@
+import type { Context } from "../context";
+
+export const rangResolvers = {
+  Query: {
+    rangs: (_: unknown, __: unknown, ctx: Context) => ctx.prisma.rang.findMany(),
+  },
+
+  Mutation: {
+    createRang: (_: unknown, args: { rang: string }, ctx: Context) =>
+      ctx.prisma.rang.create({ data: { rang: args.rang } }),
+
+    updateRang: (_: unknown, args: { rangId: string; rang: string }, ctx: Context) =>
+      ctx.prisma.rang.update({ where: { rangId: args.rangId }, data: { rang: args.rang } }),
+
+    deleteRang: async (_: unknown, args: { rangId: string }, ctx: Context) => {
+      try {
+        await ctx.prisma.rang.delete({ where: { rangId: args.rangId } });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+};
